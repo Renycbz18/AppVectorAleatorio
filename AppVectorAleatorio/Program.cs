e9ba35de1e6bcdfe2043e2cda672cef000047cc7
Renycbz18 <reny.chila@pucese.edu.ec>
2018-12-17 08:58:12 -0500

    RANDOOOM

--- AppVectorAleatorio/Program.cs
+++ AppVectorAleatorio/Program.cs
@@ -10,6 +10,24 @@ namespace AppVectorAleatorio
     {
         static void Main(string[] args)
         {
+            int[] x = new int[MAX];
+            LeerVector(x);
+            Console.WriteLine("\n ELEMENTOS DEL VECTOR");
+        }
+
+        public static void LeerVector(int[] x)
+        {
+            for (int i = 0;i<MAX; i++)
+            {
+                var randomNumber = new Random().Next(1, 5000);
+                x[i] = randomNumber;
+            }
+        }
+
+        public static void ImprimirVector (int[] x)
+        {
+            for (int i = 0; i < MAX; i++)
+                Console.WriteLine("ELEMENTO {0}= {1}",)
         }
     }
 }
